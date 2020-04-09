414d216df755fb007b5a988c83da0fa8d358da40
nspeer12 <nspeer252@gmail.com>
2020-04-09 16:37:06 -0400

    working on reinforcement learning

--- agent.py
+++ agent.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 import os
 from PIL import Image
 import cv2
-
+from gomoku import *
 
 DISCOUNT = 0.99
 REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
@@ -37,14 +37,14 @@ SHOW_PREVIEW = False
 
 
 class Agent:
-    def __init__(self):
+    def __init__(self, env):
         # main model
         # train every step
-        self.model = self.create_model()
+        self.model = self.create_model(env)
 
         # target model
         # this is what we predict against every step
-        self.target_model = self.create_model()
+        self.target_model = self.create_model(env)
         self.target_model.set_weights(self.model.get_weights())
 
         # part of the batching process
@@ -53,19 +53,19 @@ class Agent:
 
         self.target_update_counter = 0
 
-    def create_model(self):
+    def create_model(self, env):
         model = Sequential()
-        model.add(Conv2D(256, (3,3), input_shape=env.OBSERVATION_SPACE_VALUES))
+        model.add(Conv2D(64, (3,3), input_shape=env.OBSERVATION_SPACE))
         model.add(Activation("relu"))
         model.add(MaxPooling2D(2,2))
         model.add(Dropout(0.2))
-        model.add(Conv2D(256, (3,3)))
+        model.add(Conv2D(64, 3))
         model.add(Activation("relu"))
         model.add(MaxPooling2D(2,2))
         model.add(Dropout(0.2))
         model.add(Flatten())
         model.add(Dense(64))
-        model.add(Dense(env.ACTION_SPACE_SIZE, activation="linear"))
+        model.add(Dense(env.ACTION_SPACE, activation="linear"))
         model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
         return model
 
@@ -74,9 +74,9 @@ class Agent:
     
     def get_qs(self, state):
         # this is used for rgb image data, perhaps remove the /255
-        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
+        return self.model.predict(np.array(state).reshape(-1, *state.shape))[0]
 
-    def train(self, terminal_state, step):
+    def train(self, env, terminal_state, step):
         # check if we should actually train
         # this line prevents overfitting to too little memory
         if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
@@ -86,7 +86,7 @@ class Agent:
         minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
 
         # get the current q states
-        current_states = np.array([transition[0] for transition in minibatch])/255
+        current_states = env.get_board()
         current_qs_list = self.model.predict(current_states)
 
         new_current_states = np.array([transition[3] for transition in minibatch])/255
@@ -127,209 +127,70 @@ class Agent:
             self.target_update_counter = 0
 
 
-class Blob:
-    def __init__(self, size):
-        self.size = size
-        self.x = np.random.randint(0, size)
-        self.y = np.random.randint(0, size)
 
-    def __str__(self):
-        return f"Blob ({self.x}, {self.y})"
-
-    def __sub__(self, other):
-        return (self.x-other.x, self.y-other.y)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def action(self, choice):
-        '''
-        Gives us 9 total movement options. (0,1,2,3,4,5,6,7,8)
-        '''
-        if choice == 0:
-            self.move(x=1, y=1)
-        elif choice == 1:
-            self.move(x=-1, y=-1)
-        elif choice == 2:
-            self.move(x=-1, y=1)
-        elif choice == 3:
-            self.move(x=1, y=-1)
-
-        elif choice == 4:
-            self.move(x=1, y=0)
-        elif choice == 5:
-            self.move(x=-1, y=0)
-
-        elif choice == 6:
-            self.move(x=0, y=1)
-        elif choice == 7:
-            self.move(x=0, y=-1)
-
-        elif choice == 8:
-            self.move(x=0, y=0)
-
-    def move(self, x=False, y=False):
-
-        # If no value for x, move randomly
-        if not x:
-            self.x += np.random.randint(-1, 2)
-        else:
-            self.x += x
-
-        # If no value for y, move randomly
-        if not y:
-            self.y += np.random.randint(-1, 2)
-        else:
-            self.y += y
-
-        # If we are out of bounds, fix!
-        if self.x < 0:
-            self.x = 0
-        elif self.x > self.size-1:
-            self.x = self.size-1
-        if self.y < 0:
-            self.y = 0
-        elif self.y > self.size-1:
-            self.y = self.size-1
-
-
-# environment
-class BlobEnv:
-    SIZE = 10
-    RETURN_IMAGES = True
-    MOVE_PENALTY = 1
-    ENEMY_PENALTY = 300
-    FOOD_REWARD = 25
-    OBSERVATION_SPACE_VALUES = (SIZE, SIZE, 3)  # 4
-    ACTION_SPACE_SIZE = 9
-    PLAYER_N = 1  # player key in dict
-    FOOD_N = 2  # food key in dict
-    ENEMY_N = 3  # enemy key in dict
-    # the dict! (colors)
-    d = {1: (255, 175, 0),
-         2: (0, 255, 0),
-         3: (0, 0, 255)}
-
-    def reset(self):
-        self.player = Blob(self.SIZE)
-        self.food = Blob(self.SIZE)
-        while self.food == self.player:
-            self.food = Blob(self.SIZE)
-        self.enemy = Blob(self.SIZE)
-        while self.enemy == self.player or self.enemy == self.food:
-            self.enemy = Blob(self.SIZE)
-
-        self.episode_step = 0
-
-        if self.RETURN_IMAGES:
-            observation = np.array(self.get_image())
-        else:
-            observation = (self.player-self.food) + (self.player-self.enemy)
-        return observation
-
-    def step(self, action):
-        self.episode_step += 1
-        self.player.action(action)
-
-        #### MAYBE ###
-        #enemy.move()
-        #food.move()
-        ##############
-
-        if self.RETURN_IMAGES:
-            new_observation = np.array(self.get_image())
-        else:
-            new_observation = (self.player-self.food) + (self.player-self.enemy)
-
-        if self.player == self.enemy:
-            reward = -self.ENEMY_PENALTY
-        elif self.player == self.food:
-            reward = self.FOOD_REWARD
-        else:
-            reward = -self.MOVE_PENALTY
-
-        done = False
-        if reward == self.FOOD_REWARD or reward == -self.ENEMY_PENALTY or self.episode_step >= 200:
-            done = True
-
-        return new_observation, reward, done
-
-    def render(self):
-        img = self.get_image()
-        img = img.resize((300, 300))  # resizing so we can see our agent in all its glory.
-        cv2.imshow("image", np.array(img))  # show it!
-        cv2.waitKey(1)
-
-    # FOR CNN #
-    def get_image(self):
-        env = np.zeros((self.SIZE, self.SIZE, 3), dtype=np.uint8)  # starts an rbg of our size
-        env[self.food.x][self.food.y] = self.d[self.FOOD_N]  # sets the food location tile to green color
-        env[self.enemy.x][self.enemy.y] = self.d[self.ENEMY_N]  # sets the enemy location to red
-        env[self.player.x][self.player.y] = self.d[self.PLAYER_N]  # sets the player tile to blue
-        img = Image.fromarray(env, 'RGB')  # reading to rgb. Apparently. Even tho color definitions are bgr. ???
-        return img
-
-
-env = BlobEnv()
-
-# For stats
-ep_rewards = [-200]
-
-# For more repetitive results
-random.seed(1)
-np.random.seed(1)
-tf.random.set_seed(1)
-
+def main():
 # Memory fraction, used mostly when trai8ning multiple agents
 #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
 #backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
+    epsilon = 1  # not a constant, going to be decayed
 
-# Create models folder
-if not os.path.isdir('models'):
-    os.makedirs('models')
+    env = Gomoku()
+    env.print_state()
+    # track rewards
+    rewards = [0]
+    
+    random.seed(1)
+    np.random.seed(1)
+    tf.random.set_seed(1)
 
+    if not os.path.isdir('models'):
+        os.makedirs('models')
 
-agent = Agent()
+    agent = Agent(env)
+    agentcp = agent
 
-for episode in tqdm(range(1, EPISODES+1), ascii=True, unit="episode"):
-    agent.tensorboard.step = episode
+    for episode in tqdm(range(1, EPISODES+1), ascii=True, unit="episode"):
+        agent.tensorboard.step = episode
 
-    episode_reward = 0
-    step = 1
-    current_state = env.reset()
-    done = False
+        episode_reward = 0
+        episode_step = 1
+        current_state = env.reset()
+        done = False
 
-    while not done:
-        if np.random.random() > epsilon:
-            action = np.argmax(agent.get_qs(current_state))
-        else:
-            action = np.random.randint(0, env.ACTION_SPACE_SIZE)
-        
-        new_state, reward, done = env.step(action)
-        episode_reward += reward
+        while not done:
+            if np.random.random() > epsilon:
+                action = np.argmax(agent.get_qs(current_state))
+            else:
+                action = np.random.randint(0, env.ACTION_SPACE)
+            
+            new_state, reward, done = env.step(1, action)
+            episode_reward += reward
 
-        if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
-            env.render()
-        
-        agent.update_replay_memory((current_state, action, reward, new_state, done))
-        agent.train(done, step)
-        
-        current_state = new_state 
-        step += 1
-        
-        # Append episode reward to a list and log stats (every given number of episodes)
-    ep_rewards.append(episode_reward)
-    if not episode % AGGREGATE_STATS_EVERY or episode == 1:
-        average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        #agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+            if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+                env.render()
+            
+            agent.update_replay_memory((current_state, action, reward, new_state, done))
+            agent.train(env, done, episode_step)
+            
+            current_state = new_state 
+            episode_step += 1
+            
+            # Append episode reward to a list and log stats (every given number of episodes)
+        ep_rewards.append(episode_reward)
+        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+            average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            #agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
 
-        # Save model, but only when min reward is greater or equal a set value
-        if min_reward >= MIN_REWARD:
-            agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+            # Save model, but only when min reward is greater or equal a set value
+            if min_reward >= MIN_REWARD:
+                agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
 
-    # Decay epsilon
-    if epsilon > MIN_EPSILON:
-        epsilon *= EPSILON_DECAY
-        epsilon = max(MIN_EPSILON, epsilon)
+        # Decay epsilon
+        if epsilon > MIN_EPSILON:
+            epsilon *= EPSILON_DECAY
+            epsilon = max(MIN_EPSILON, epsilon)
+
+if __name__ == '__main__':
+    main()
--- gomoku.py
+++ gomoku.py
@@ -1,29 +1,74 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 class Occupied(Exception):
     def __init__(self, x, y):
         print('Error: Board location (',x,',',y,') is already taken')
         exit(0)
 
-class Board:
-    def __init__(self, n):
-        self.n = n
-        self.board = np.zeros((n, n), dtype=int)
-        self.winner = None
+class Gomoku:
+    SIZE = 10
+    OBSERVATION_SPACE = (SIZE, SIZE, 1)
+    ACTION_SPACE = SIZE * SIZE
+    MOVE_PENALTY = -1
+    LOSS_PENALTY = -500
+    WIN_REWARD = 1000
+    INVALID_MOVE_PENALTY = -5000
+    PLAYER0 = 0
+    PLAYER1 = 1
+    EMPTY_SPACE = -1
+
+    def __init__(self):
+        self.board = np.full((self.SIZE, self.SIZE), self.EMPTY_SPACE, dtype=int)
+        self.winner = -69
+        self.episode_step = 0
     
-    def place(self, player, x, y):
-        if self.board[y][x] == 0:
-            self.board[y][x] = player
+    def action_to_coord(self, action):
+        y = action % self.SIZE
+        x = int((action / self.SIZE) % self.SIZE)
+        return x, y
+
+    def step(self, player, action):
+        self.episode_step += 1
+        observation = self.get_board()
+
+        x, y = self.action_to_coord(action)
+        print('ACTION:', x,y)
+        reward = self.place(player, x, y)
+
+        if self.winner == player:
+            done = True
+            reward = WIN_REWARD
+            return obervation, reward, done
         else:
-            raise Occupied(x, y)
+            done = False
+            return observation, reward, done
     
+    def get_board(self):
+        return self.board
+
+    def show(self):
+        plt.imshow(self.board)
+        plt.show()
+
+    def place(self, player, x, y):
+        if self.board[y][x] == self.EMPTY_SPACE:
+            self.board[y][x] = player
+            return self.MOVE_PENALTY
+        else:
+            return self.INVALID_MOVE_PENALTY
+            #raise Occupied(x, y)
+    
+    def reset(self):
+        self.board = np.full((self.SIZE, self.SIZE), self.EMPTY_SPACE, dtype=int)
+        self.winner = -69
+        self.episode_step = 0
+
     def check_state(self):
         # check vertical
-        for x in range(0, self.n-4):
-            for y in range(0, self.n-4):
-                if self.board[y][x] != 0:
+        for x in range(0, SIZE):
+            for y in range(0, SIZE-4):
+                if self.board[y][x] != EMPTY_SPACE:
                     curplayer = self.board[y][x]
                     score = 1
                     for j in range(4):
@@ -37,8 +82,8 @@ class Board:
                             return curplayer
 
         # check horizontal
-        for x in range(0, self.n-4):
-            for y in range(0, self.n-4):
+        for x in range(0, SIZE-4):
+            for y in range(0, SIZE-4):
                 if self.board[y][x] != 0:
                     curplayer = self.board[y][x]
                     score = 1
@@ -53,8 +98,8 @@ class Board:
                             return curplayer
         
         # check diagonal \
-        for x in range(0, self.n-4):
-            for y in range(0, self.n-4):
+        for x in range(0, SIZE-4):
+            for y in range(0, SIZE-4):
                 if self.board[y][x] != 0:
                     curplayer = self.board[y][x]
                     score = 1
@@ -69,8 +114,8 @@ class Board:
                             return curplayer
         
         # check diagonal /
-        for x in range(0, self.n-4):
-            for y in reversed(range(4, self.n)):
+        for x in range(0, SIZE-4):
+            for y in reversed(range(4, SIZE)):
                 if self.board[y][x] != 0:
                     curplayer = self.board[y][x]
                     score = 1
@@ -88,16 +133,3 @@ class Board:
 
     def print_state(self):
         print(self.board)
-
-def main():
-    n = 10
-    game = Board(n)
-    for i in range(10):
-        game.place(1,9-i,i)
-
-    print(game.check_state())
-    game.print_state()
-
-
-if __name__ == '__main__':
-    main()
