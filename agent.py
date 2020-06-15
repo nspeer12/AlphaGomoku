5f1e23720370fc344e85aeba133e49e8214a7fc7
nspeer <nspeer252@gmail.com>
2020-06-15 15:22:29 -0500

    separated agent and environment (gomoku) and added models

--- agent.py
+++ agent.py
@@ -1,196 +1,107 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.optimizers import Adam
-from modifiedtfboard import ModifiedTensorBoard
-from collections import deque
-import time
-import random
-from tqdm import tqdm
-import os
-from PIL import Image
-import cv2
-from gomoku import *
+from gomoku import State
+import pickle
 
-DISCOUNT = 0.99
-REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
-MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
-MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
-UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
-MODEL_NAME = '2x256'
-MIN_REWARD = -200  # For model save
-MEMORY_FRACTION = 0.20
+N = 10
+class Player:
+    def __init__(self, name, exp_rate=0.3):
+        self.name = name
+        self.states = []  # record all positions taken
+        self.lr = 0.2
+        self.exp_rate = exp_rate
+        self.decay_gamma = 0.9
+        self.states_value = {}  # state -> value
 
-# Environment settings
-EPISODES = 20_000
+    def getHash(self, board):
+        boardHash = str(board.reshape(N * N))
+        return boardHash
 
-# Exploration settings
-epsilon = 1  # not a constant, going to be decayed
-EPSILON_DECAY = 0.99975
-MIN_EPSILON = 0.001
-
-#  Stats settings
-AGGREGATE_STATS_EVERY = 50  # episodes
-SHOW_PREVIEW = False
-
-
-class Agent:
-    def __init__(self, env):
-        # main model
-        # train every step
-        self.model = self.create_model(env)
-
-        # target model
-        # this is what we predict against every step
-        self.target_model = self.create_model(env)
-        self.target_model.set_weights(self.model.get_weights())
-
-        # part of the batching process
-        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
-        self.tensorboard = TensorBoard(log_dir=f"logs\{MODEL_NAME}-{int(time.time())}", histogram_freq=1)
-
-        self.target_update_counter = 0
-
-    def create_model(self, env):
-        model = Sequential()
-        model.add(Conv2D(64, (3,3), input_shape=env.OBSERVATION_SPACE))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(2,2))
-        model.add(Dropout(0.2))
-        model.add(Conv2D(64, 3))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(2,2))
-        model.add(Dropout(0.2))
-        model.add(Flatten())
-        model.add(Dense(64))
-        model.add(Dense(env.ACTION_SPACE, activation="linear"))
-        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
-        return model
-
-    def update_replay_memory(self, transition):
-        self.replay_memory.append(transition)
-    
-    def get_qs(self, state):
-        # this is used for rgb image data, perhaps remove the /255
-        return self.model.predict(np.array(state).reshape(-1, *state.shape))[0]
-
-    def train(self, env, terminal_state, step):
-        # check if we should actually train
-        # this line prevents overfitting to too little memory
-        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
-            return
-
-        # grab a random mini batch
-        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
-
-        # get the current q states
-        current_states = env.get_board()
-        current_qs_list = self.model.predict(current_states)
-
-        new_current_states = np.array([transition[3] for transition in minibatch])/255
-        future_qs_list = self.target_model.predict(new_current_states)
-        
-        # features
-        X = []
-        # labels
-        y = []
-
-        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
-            if not done:
-                max_future_q = np.max(future_qs_list[index])
-                new_q = reward + DISCOUNT * max_future_q
-            else:
-                new_q = reward
-            
-            current_qs = current_qs_list[index]
-            current_qs[action] = new_q
-            X.append(current_state)
-            y.append(current_qs)
-        
-        # only fit on terminal state
-        if step % 10 == 0:
-            self.model.fit(np.array(X), np.array(y), batch_size = MINIBATCH_SIZE,
-                           verbose=0, shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
+    def chooseAction(self, positions, current_board, symbol):
+        if np.random.uniform(0, 1) <= self.exp_rate:
+            # take random action
+            idx = np.random.choice(len(positions))
+            action = positions[idx]
         else:
-            self.model.fit(np.array(X), np.array(y), batch_size = MINIBATCH_SIZE,
-                           verbose=0, shuffle=False)
-        
-        # updating target model counter
-        if terminal_state:
-            self.target_update_counter += 1
-        
-        # update target model
-        if self.target_update_counter > UPDATE_TARGET_EVERY:
-            self.target_model.set_weights(self.model.get_weights())
-            self.target_update_counter = 0
+            value_max = -999
+            for p in positions:
+                next_board = current_board.copy()
+                next_board[p] = symbol
+                next_boardHash = self.getHash(next_board)
+                value = 0 if self.states_value.get(next_boardHash) is None else self.states_value.get(next_boardHash)
+                # print("value", value)
+                if value >= value_max:
+                    value_max = value
+                    action = p
+        # print("{} takes action {}".format(self.name, action))
+        return action
+
+    # append a hash state
+    def addState(self, state):
+        self.states.append(state)
+
+    # at the end of game, backpropagate and update states value
+    def feedReward(self, reward):
+        for st in reversed(self.states):
+            if self.states_value.get(st) is None:
+                self.states_value[st] = 0
+            self.states_value[st] += self.lr * (self.decay_gamma * reward - self.states_value[st])
+            reward = self.states_value[st]
+
+    def reset(self):
+        self.states = []
+
+    def savePolicy(self, rounds):
+        fw = open(str(N) +'_dim_'+ str(rounds) + '_rounds', 'wb')
+        pickle.dump(self.states_value, fw)
+        fw.close()
+
+    def loadPolicy(self, file):
+        fr = open(file, 'rb')
+        self.states_value = pickle.load(fr)
+        fr.close()
 
 
+class HumanPlayer:
+    def __init__(self, name):
+        self.name = name
 
-def main():
-# Memory fraction, used mostly when trai8ning multiple agents
-#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
-#backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
-    epsilon = 1  # not a constant, going to be decayed
+    def chooseAction(self, positions):
+        while True:
+            row = int(input("Input your action row:"))
+            col = int(input("Input your action col:"))
+            action = (row, col)
+            if action in positions:
+                return action
 
-    env = Gomoku()
-    env.print_state()
-    # track rewards
-    rewards = [0]
-    
-    random.seed(1)
-    np.random.seed(1)
-    tf.random.set_seed(1)
+    # append a hash state
+    def addState(self, state):
+        pass
 
-    if not os.path.isdir('models'):
-        os.makedirs('models')
+    # at the end of game, backpropagate and update states value
+    def feedReward(self, reward):
+        pass
 
-    agent = Agent(env)
-    agentcp = agent
+    def reset(self):
+        pass
 
-    for episode in tqdm(range(1, EPISODES+1), ascii=True, unit="episode"):
-        agent.tensorboard.step = episode
 
-        episode_reward = 0
-        episode_step = 1
-        current_state = env.reset()
-        done = False
+if __name__ == "__main__":
+    # training
+    p1 = Player("p1")
+    p2 = Player("p2")
 
-        while not done:
-            if np.random.random() > epsilon:
-                action = np.argmax(agent.get_qs(current_state))
-            else:
-                action = np.random.randint(0, env.ACTION_SPACE)
-            
-            new_state, reward, done = env.step(1, action)
-            episode_reward += reward
+    st = State(p1, p2)
+    print("training...")
+    rounds = 10000
+    st.play(rounds)
+    p1.savePolicy(rounds)
 
-            if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
-                env.render()
-            
-            agent.update_replay_memory((current_state, action, reward, new_state, done))
-            agent.train(env, done, episode_step)
-            
-            current_state = new_state 
-            episode_step += 1
-            
-            # Append episode reward to a list and log stats (every given number of episodes)
-        ep_rewards.append(episode_reward)
-        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
-            average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            #agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+    # play with human
+    p1 = Player("computer", exp_rate=0)
+    p1.loadPolicy("models/10_dim_10000_rounds")
 
-            # Save model, but only when min reward is greater or equal a set value
-            if min_reward >= MIN_REWARD:
-                agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+    p2 = HumanPlayer("human")
 
-        # Decay epsilon
-        if epsilon > MIN_EPSILON:
-            epsilon *= EPSILON_DECAY
-            epsilon = max(MIN_EPSILON, epsilon)
+    st = State(p1, p2)
+    st.play2()
 
-if __name__ == '__main__':
-    main()
--- gomoku.py
+++ gomoku.py
@@ -1,135 +1,240 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import pickle
+from tqdm import tqdm
 
-class Occupied(Exception):
-    def __init__(self, x, y):
-        print('Error: Board location (',x,',',y,') is already taken')
-        exit(0)
+N = 10
+IN_A_ROW = 3
+EMPTY_SPACE = 0
 
-class Gomoku:
-    SIZE = 10
-    OBSERVATION_SPACE = (SIZE, SIZE, 1)
-    ACTION_SPACE = SIZE * SIZE
-    MOVE_PENALTY = -1
-    LOSS_PENALTY = -500
-    WIN_REWARD = 1000
-    INVALID_MOVE_PENALTY = -5000
-    PLAYER0 = 0
-    PLAYER1 = 1
-    EMPTY_SPACE = -1
+class State:
+    def __init__(self, p1, p2):
+        self.board = np.zeros((N, N))
+        self.p1 = p1
+        self.p2 = p2
+        self.isEnd = False
+        self.boardHash = None
+        # init p1 plays first
+        self.playerSymbol = 1
+        self.winner = None
 
-    def __init__(self):
-        self.board = np.full((self.SIZE, self.SIZE), self.EMPTY_SPACE, dtype=int)
-        self.winner = -69
-        self.episode_step = 0
+    # get unique hash of current board state
+    def getHash(self):
+        self.boardHash = str(self.board.reshape(N * N))
+        return self.boardHash
     
-    def action_to_coord(self, action):
-        y = action % self.SIZE
-        x = int((action / self.SIZE) % self.SIZE)
-        return x, y
-
-    def step(self, player, action):
-        self.episode_step += 1
-        observation = self.get_board()
-
-        x, y = self.action_to_coord(action)
-        print('ACTION:', x,y)
-        reward = self.place(player, x, y)
-
-        if self.winner == player:
-            done = True
-            reward = WIN_REWARD
-            return obervation, reward, done
-        else:
-            done = False
-            return observation, reward, done
-    
-    def get_board(self):
-        return self.board
-
-    def show(self):
-        plt.imshow(self.board)
-        plt.show()
-
-    def place(self, player, x, y):
-        if self.board[y][x] == self.EMPTY_SPACE:
-            self.board[y][x] = player
-            return self.MOVE_PENALTY
-        else:
-            return self.INVALID_MOVE_PENALTY
-            #raise Occupied(x, y)
-    
-    def reset(self):
-        self.board = np.full((self.SIZE, self.SIZE), self.EMPTY_SPACE, dtype=int)
-        self.winner = -69
-        self.episode_step = 0
-
-    def check_state(self):
+    def check_state(self, print_game=False):
+        
         # check vertical
-        for x in range(0, SIZE):
-            for y in range(0, SIZE-4):
-                if self.board[y][x] != EMPTY_SPACE:
-                    curplayer = self.board[y][x]
+        for j in range(0, N):
+            for i in range(0, N - IN_A_ROW):
+                if self.board[i][j] != EMPTY_SPACE:
+                    curplayer = self.board[i][j]
                     score = 1
-                    for j in range(4):
-                        if self.board[y+j][x] == curplayer:
+                    for k in range(i+1, i+IN_A_ROW):
+                        if self.board[k][j] == curplayer:
                             score+=1
                         else:
                             break
 
-                        if score == 5:
-                            print(curplayer, ' wins!')
+                        if score == IN_A_ROW:
+                            if print_game:
+                                print(curplayer, ' wins!')
                             return curplayer
 
         # check horizontal
-        for x in range(0, SIZE-4):
-            for y in range(0, SIZE-4):
-                if self.board[y][x] != 0:
-                    curplayer = self.board[y][x]
+        for i in range(0, N):
+            for j in range(0, N - IN_A_ROW):
+                if self.board[i][j] != EMPTY_SPACE:
+                    curplayer = self.board[i][j]
                     score = 1
-                    for j in range(4):
-                        if self.board[y][x+j] == curplayer:
+                    for k in range(j+1, j+IN_A_ROW):
+                        if self.board[i][k] == curplayer:
                             score+=1
                         else:
                             break
 
-                        if score == 5:
-                            print(curplayer, ' wins!')
+                        if score == IN_A_ROW:
+                            if print_game:
+                                print(curplayer, ' wins!')
                             return curplayer
-        
+
         # check diagonal \
-        for x in range(0, SIZE-4):
-            for y in range(0, SIZE-4):
-                if self.board[y][x] != 0:
-                    curplayer = self.board[y][x]
+        for i in range(0, N - IN_A_ROW):
+            for j in range(0, N - IN_A_ROW):
+                if self.board[i][j] != EMPTY_SPACE:
+                    curplayer = self.board[i][j]
                     score = 1
-                    for j in range(4):
-                        if self.board[y+j][x+j] == curplayer:
+                    for k in range(1, IN_A_ROW):
+                        if self.board[j+k][i+k] == curplayer:
                             score+=1
                         else:
                             break
 
-                        if score == 5:
-                            print(curplayer, ' wins!')
+                        if score == IN_A_ROW:
+                            if print_game:
+                                print(curplayer, ' wins!')
                             return curplayer
-        
+
         # check diagonal /
-        for x in range(0, SIZE-4):
-            for y in reversed(range(4, SIZE)):
-                if self.board[y][x] != 0:
-                    curplayer = self.board[y][x]
+        for j in range(0, N - IN_A_ROW):
+            for i in reversed(range(IN_A_ROW, N)):
+                if self.board[i][j] != EMPTY_SPACE:
+                    curplayer = self.board[i][j]
                     score = 1
-                    for j in range(4):
-                        if self.board[y-j][x+j] == curplayer:
+                    for k in range(1, IN_A_ROW):
+                        if self.board[i-k][j+k] == curplayer:
                             score+=1
                         else:
                             break
 
-                        if score == 5:
-                            print(curplayer, ' wins!')
+                        if score == IN_A_ROW:
+                            if print_game:
+                                print(curplayer, ' wins!')
                             return curplayer
+            
+        # tie
+        # no available positions
+        if len(self.availablePositions()) == 0:
+            self.isEnd = True
+            return 0
+        # not end
+        self.isEnd = False
+        return None
 
-        return False
+    def availablePositions(self):
+        positions = []
+        for i in range(N):
+            for j in range(N):
+                if self.board[i, j] == EMPTY_SPACE:
+                    positions.append((i, j))  # need to be tuple
+        return positions
 
-    def print_state(self):
-        print(self.board)
+    def updateState(self, position):
+        self.board[position] = self.playerSymbol
+        # switch to another player
+        self.playerSymbol = -1 if self.playerSymbol == 1 else 1
+
+    # only when game ends
+    def giveReward(self):
+        result = self.check_state()
+        # backpropagate reward
+        if result == 1:
+            self.p1.feedReward(1)
+            self.p2.feedReward(0)
+        elif result == -1:
+            self.p1.feedReward(0)
+            self.p2.feedReward(1)
+        else:
+            self.p1.feedReward(0.1)
+            self.p2.feedReward(0.5)
+
+    # board reset
+    def reset(self):
+        self.board = np.zeros((N, N))
+        self.boardHash = None
+        self.isEnd = False
+        self.playerSymbol = 1
+
+    def play(self, rounds=100):
+        for i in tqdm(range(rounds)):
+            while not self.isEnd:
+                # Player 1
+                positions = self.availablePositions()
+                p1_action = self.p1.chooseAction(positions, self.board, self.playerSymbol)
+                # take action and upate board state
+                self.updateState(p1_action)
+                board_hash = self.getHash()
+                self.p1.addState(board_hash)
+                # check board status if it is end
+
+                win = self.check_state()
+                if win is not None:
+                    # self.showBoard()
+                    # ended with p1 either win or draw
+                    self.giveReward()
+                    self.p1.reset()
+                    self.p2.reset()
+                    self.reset()
+                    break
+
+                else:
+                    # Player 2
+                    positions = self.availablePositions()
+                    p2_action = self.p2.chooseAction(positions, self.board, self.playerSymbol)
+                    self.updateState(p2_action)
+                    board_hash = self.getHash()
+                    self.p2.addState(board_hash)
+
+                    win = self.check_state()
+                    if win is not None:
+                        # self.showBoard()
+                        # ended with p2 either win or draw
+                        self.giveReward()
+                        self.p1.reset()
+                        self.p2.reset()
+                        self.reset()
+                        break
+
+    # play with human
+    def play2(self):
+        while not self.isEnd:
+            # Player 1
+            positions = self.availablePositions()
+            p1_action = self.p1.chooseAction(positions, self.board, self.playerSymbol)
+            # take action and upate board state
+            self.updateState(p1_action)
+            self.showBoard()
+            # check board status if it is end
+            win = self.check_state()
+            if win is not None:
+                if win == 1:
+                    print(self.p1.name, "wins!")
+                else:
+                    print("tie!")
+                self.reset()
+                break
+
+            else:
+                # Player 2
+                positions = self.availablePositions()
+                p2_action = self.p2.chooseAction(positions)
+
+                self.updateState(p2_action)
+                self.showBoard()
+                win = self.check_state()
+                if win is not None:
+                    if win == -1:
+                        print(self.p2.name, "wins!")
+                    else:
+                        print("tie!")
+                    self.reset()
+                    break
+
+    def showBoard(self):
+        # p1: x  p2: o
+        for k in range(N):
+            if k == 0:
+                print('   ', end='')
+            print(' ', k, '', end='' if k != N-1 else '\n')
+            
+        for k in range(N):
+            if k == 0:
+                print('   ', end='')
+            print('----', end='' if k != N-1 else '-\n')
+        
+        for i in range(0, N):
+            print(i, ' ', end='')
+            out = '| '
+            for j in range(0, N):
+                if self.board[i, j] == 1:
+                    token = 'x'
+                if self.board[i, j] == -1:
+                    token = 'o'
+                if self.board[i, j] == 0:
+                    token = ' '
+                out += token + ' | '
+            print(out)
+            for k in range(N):
+                if k == 0:
+                    print('   ', end='')
+                print('----', end='' if k != N-1 else '-\n')
